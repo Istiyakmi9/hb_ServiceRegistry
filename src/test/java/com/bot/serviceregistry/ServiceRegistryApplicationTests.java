8ffec37feba73cc7da84266c5faccb869d1cee3e
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-10 11:31:30 +0530

    registry code

---
+++ src/test/java/com/bot/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
